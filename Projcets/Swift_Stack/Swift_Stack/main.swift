471bb33b8d2fc003ddd0181ce23d06bfbf07995f
darcy97 <darcy97zh@gmail.com>
2017-04-30 15:26:45 +0800

    stack bt swift

---
+++ Projcets/Swift_Stack/Swift_Stack/main.swift
@@ -0,0 +1,61 @@
+//
+//  main.swift
+//  Swift_Stack
+//
+//  Created by 张皓 on 2017/4/30.
+//  Copyright © 2017年 Darcy. All rights reserved.
+//
+
+import Foundation
+
+/// 使用数组定义栈
+class Stack {
+    var stack: [AnyObject]
+    
+    init() {
+        stack = [AnyObject]()
+    }
+    
+    func push(object: AnyObject) {
+        stack.append(object)
+    }
+    
+    func pop() -> AnyObject? {
+        if !isEmpty() {
+            return stack.removeLast()
+        } else {
+            return nil
+        }
+    }
+    
+    func isEmpty() -> Bool {
+        return stack.isEmpty
+    }
+    
+    func peek() -> AnyObject? {
+        return stack.last
+    }
+    
+    func size() -> Int {
+        return stack.count
+    }
+}
+
+
+let myStack = Stack.init()
+
+myStack.push(object: "hello" as AnyObject)
+
+myStack.push(object: "hello swift" as AnyObject)
+
+myStack.push(object: "hello stack" as AnyObject)
+
+print(myStack.isEmpty())
+print(myStack.pop()!)
+print(myStack.peek()!)
+print(myStack.size())
+print(myStack.isEmpty())
+
+
+
+
